0cd153f10fde370df9539d3f6e3576bac0393438
Kenneth Bergquist <bergquist.kenneth@gmail.com>
2014-10-30 12:23:19 -0400

    fix dumb mistake

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
 	belongs_to :yak
 	acts_as_votable
+
+	validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
 	has_many :yaks
 	acts_as_voter
-
-	validates :body, presence: true
 end
